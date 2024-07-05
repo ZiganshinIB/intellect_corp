ef131b729a51331b64f1e62c76aedb020eb01225
ZiganshinIB <elzig3012@gmail.com>
2024-07-05 13:27:14 +0300

    Example work with AD

--- icorp/worker/ad/main.py
+++ icorp/worker/ad/main.py
@@ -1,7 +1,25 @@
 from ldap3 import Server, Connection, ALL, NTLM
 # from django.conf import settings
 from icorp.icorp import settings
-def main():
+
+
+def search():
+    conn.search('dc=kzp,dc=in',
+                '(sAMAccountName=ilmir.ziganshin)',
+                attributes=['OU', 'cn', 'sAMAccountName', 'userPrincipalName', 'objectClass', 'memberOf'])
+    print(conn.entries)
+
+def create_user(first_name, last_name, p,user_name, password):
+    p = f" {p}" if p else ''
+    cn = f"{last_name} {first_name}{p}"
+    conn.add(f'cn={p},ou=ldap3-tutorial,dc=demo1,dc=freeipa,dc=org', 'inetOrgPerson',
+             {'givenName': 'Beatrix', 'sn': 'Young', 'departmentNumber': 'DEV', 'telephoneNumber': 1111})
+
+
+if __name__ == '__main__':
+    from environs import Env
+    env = Env()
+    env.read_env()
     server = Server(env.str("AD_HOST"), get_info=ALL)
 
     conn = Connection(server,
@@ -9,17 +27,4 @@ def main():
                       password=env.str("AD_ADMIN_PASSWORD"),
                       authentication=NTLM)
     conn.bind()
-    # print(server.schema)
-    # print(conn)
-    # print(conn.extend.standard.who_am_i())
-    conn.search('dc=kzp,dc=in',
-                '(objectClass=*)',
-                attributes=['cn', 'sAMAccountName', 'userPrincipalName', 'objectClass', 'memberOf'])
-    print(conn.entries[100:150])
-
-
-if __name__ == '__main__':
-    from environs import Env
-    env = Env()
-    env.read_env()
-    main()
+    search()
