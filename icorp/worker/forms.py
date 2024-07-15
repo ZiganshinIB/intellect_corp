32468b986c692fe9fb83be8aaa4517d63cca7c28
ZiganshinIB <elzig3012@gmail.com>
2024-07-15 20:44:43 +0300

    Add form

--- icorp/worker/forms.py
+++ icorp/worker/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from phonenumber_field.formfields import PhoneNumberField
 from .models import Profile
 
 
@@ -33,6 +33,39 @@ class ProfileForm(forms.ModelForm):
         widget=forms.TextInput(
             attrs={
                 'placeholder': 'Отчество',
+                'class': 'form-control',
+            })
+    )
+
+    birthday = forms.DateField(
+        label=False,
+        required=True,
+        error_messages={'required': 'Укажите дату рождения'},
+        widget=forms.DateInput(
+            attrs={
+                'placeholder': 'Дата рождения',
+                'class': 'form-control',
+            })
+    )
+
+    telephone = PhoneNumberField(
+        label=False,
+        required=True,
+        error_messages={'required': 'Укажите номер телефона'},
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Номер телефона',
+                'class': 'form-control',
+            })
+    )
+
+    data_start_work = forms.DateField(
+        label=False,
+        required=True,
+        error_messages={'required': 'Укажите дату начала работы'},
+        widget=forms.DateInput(
+            attrs={
+                'placeholder': 'Дата начала работы',
                 'class': 'form-control',
             })
     )
